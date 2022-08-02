96bdec0658ad103fa61082f84e7f8a8666a2b06c
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-08-01 21:13:51 -0400

    add panel assets

---
+++ v2/panel/plugins/custom/datatables/datatables.js
@@ -0,0 +1,233 @@
+$("#kt_datatable_licenses").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_users").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_subs").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_chats").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_messages").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_sessions").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_webhooks").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});
+
+$("#kt_datatable_files").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});   
+
+$("#kt_datatable_vars").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});   
+
+$("#kt_datatable_logs").DataTable({
+"language": {
+"lengthMenu": "Show _MENU_",
+},
+"dom":
+"<'row'" +
+"<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+"<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+">" +
+
+"<'table-responsive'tr>" +
+
+"<'row'" +
+"<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+"<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+">"
+});   
+ 
+$("#kt_datatable_blacklists").DataTable({
+  "language": {
+  "lengthMenu": "Show _MENU_",
+  },
+  "dom":
+  "<'row'" +
+  "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+  "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+  ">" +
+  
+  "<'table-responsive'tr>" +
+  
+  "<'row'" +
+  "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+  "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+  ">"
+  });   
+
+  $("#kt_datatable_manageaccs").DataTable({
+    "language": {
+    "lengthMenu": "Show _MENU_",
+    },
+    "dom":
+    "<'row'" +
+    "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+    "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+    ">" +
+    
+    "<'table-responsive'tr>" +
+    
+    "<'row'" +
+    "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+    "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+    ">"
+    });   
+
+    $("#kt_datatable_webloader").DataTable({
+      "language": {
+      "lengthMenu": "Show _MENU_",
+      },
+      "dom":
+      "<'row'" +
+      "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+      "<'col-sm-6 d-flex align-items-center justify-content-end'f>" +
+      ">" +
+      
+      "<'table-responsive'tr>" +
+      
+      "<'row'" +
+      "<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+      "<'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+      ">"
+      });   
